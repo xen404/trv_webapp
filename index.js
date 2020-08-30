a081930a0ff86fb2842cf15fd36ad1cf10f9dd3e
Anton Mihalkevich <anton.mihalkevich.com>
2020-08-30 20:12:15 +0200

    add some stuff to index

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ require('./routes/newsRoutes')(app);
 require('./routes/usersRoutes')(app);
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will server aup production  assests 
+    //like out main.js file or main.css file
+    app.use(express.static('client/build'));
+
+
+    // Express will serve up index.html file
+    // if does not recognise the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
