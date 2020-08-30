099711e2a901c04300a3701000e592c561ce2e04
Anton Mihalkevich <anton.mihalkevich.com>
2020-08-30 18:24:28 +0200

    add keys to prod.js

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET, 
+    cloudinaryName: process.env.CLOUDINARY_NAME,
+    jwtSecret: process.env.JWT_SECRET
+}
