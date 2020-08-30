e4165ad76f4987fc89a1734bb9137e922fe66c09
Anton Mihalkevich <anton.mihalkevich.com>
2020-08-30 21:27:39 +0200

    add database configuration for heroku

--- database.js
+++ database.js
@@ -1,11 +1,22 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
+    user: "bbrcwcwkhpupmt",
+    password: "16fe9f8dc46ae8ae70e5cf1024e94a249c48e0467515dd1692455d3ff7917cac",
+    host: "ec2-46-137-79-235.eu-west-1.compute.amazonaws.com",
+    port: 5432,
+    database: "d9vfdk5ccgf989"
+});
+
+module.exports = pool;
+
+
+/*
+const pool = new Pool({
     user: "karton",
     password: "password",
     host: "localhost",
     port: 5432,
     database: "trv"
 });
-
-module.exports = pool;
+*/
