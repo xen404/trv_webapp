bb657ba7d8301abdaec9802915c53c56b88437ec
Anton Mihalkevich <anton.mihalkevich.com>
2020-08-30 18:04:27 +0200

    Add dynamic port to index.js. Add start script to package.json.

--- index.js
+++ index.js
@@ -12,6 +12,4 @@ require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log("server has started on port 5000");
-});
+app.listen(PORT);
