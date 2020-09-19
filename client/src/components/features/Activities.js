2acac0b694dfa2a932344c2170709ac37f04f5e0
Anton Mihalkevich <anton.mihalkevich.com>
2020-09-19 18:09:13 +0200

    Add activities to landing

--- client/src/components/features/Activities.js
+++ client/src/components/features/Activities.js
@@ -1,8 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-import { ReactComponent as SvgDotPatternIcon } from "../../images/dot-pattern.svg";
 import { SectionHeading as HeadingTitle } from "../misc/Headings.js";
+import rudertage from "../../images/trv/rudertage.jpeg";
+import sternfahrt from "../../images/trv/sternfahrt.jpeg";
+import wanderfahrt from "../../images/trv/wanderfahrt.jpeg";
+import jugendsport from "../../images/trv/jugendsport.jpeg";
+import wettkampfsport from "../../images/trv/wettkampfsport.jpeg";
 
 const Container = tw.div`relative`;
 
@@ -13,13 +17,13 @@ const HeadingDescription = tw.p`mt-4 font-medium text-gray-600 text-center max-w
 
 const Content = tw.div`mt-16`;
 
-const Card = styled.div(props => [
+const Card = styled.div((props) => [
   tw`mt-24 md:flex justify-center items-center`,
-  props.reversed ? tw`flex-row-reverse` : "flex-row"
+  props.reversed ? tw`flex-row-reverse` : "flex-row",
 ]);
-const Image = styled.div(props => [
+const Image = styled.div((props) => [
   `background-image: url("${props.imageSrc}");`,
-  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`
+  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`,
 ]);
 const Details = tw.div`mt-4 md:mt-0 md:max-w-md mx-4 sm:mx-8 md:mx-4 lg:mx-8`;
 const Subtitle = tw.div`font-bold tracking-wide text-secondary-100`;
@@ -27,50 +31,49 @@ const Title = tw.h4`text-3xl font-bold text-gray-900`;
 const Description = tw.p`mt-2 text-sm leading-loose`;
 const Link = tw.a`inline-block mt-4 text-sm text-primary-500 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent hover:border-primary-500`;
 
-const SvgDotPattern1 = tw(
-  SvgDotPatternIcon
-)`absolute top-0 left-0 transform -translate-x-20 rotate-90 translate-y-8 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern2 = tw(
-  SvgDotPatternIcon
-)`absolute top-0 right-0 transform translate-x-20 rotate-45 translate-y-24 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern3 = tw(
-  SvgDotPatternIcon
-)`absolute bottom-0 left-0 transform -translate-x-20 rotate-45 -translate-y-8 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern4 = tw(
-  SvgDotPatternIcon
-)`absolute bottom-0 right-0 transform translate-x-20 rotate-90 -translate-y-24 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-
 export default () => {
   const cards = [
     {
-      imageSrc:
-        "https://images.unsplash.com/photo-1550699026-4114bbf4fb49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80",
-      subtitle: "Paid",
-      title: "Loachella, NYC",
+      imageSrc: rudertage,
+      subtitle: "",
+      title: "Rudertage",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      url: "https://timerse.com"
+        "Mehrmals wöchentlich treffen wir uns, um auf der Donau und der Großen Tulln Rudern als Freizeit- und Familiensport zu betreiben. Unsere Rudertage sind von April bis September: Montag, Mittwoch und Samstag – aktuelle Zeiten siehe linke Spalte.",
+      url: "https://timerse.com",
     },
 
     {
-      imageSrc:
-        "https://images.unsplash.com/photo-1543423924-b9f161af87e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-      subtitle: "Free",
-      title: "Rock In Rio, Upstate",
+      imageSrc: sternfahrt,
+      subtitle: "",
+      title: "Sternfahrten",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      url: "https://timerse.com"
+        "Wir rudern gemeinsam zu dem Klub, der die sog. Sternfahrt veranstaltet. Nach der gemeinsamen Fahrt steht dort das gesellige Zusammensein mit Ruderern aus verschiedenen Vereinen im Mittelpunkt. Sternfahrten finden zu den Klubs an der Donau in Niederösterreich und Wien statt.",
+      url: "https://timerse.com",
     },
 
     {
-      imageSrc:
-        "https://images.unsplash.com/photo-1509824227185-9c5a01ceba0d?ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80",
-      subtitle: "Exclusive",
-      title: "Lollapalooza, Manhattan",
+      imageSrc: wanderfahrt,
+      subtitle: "",
+      title: "Wanderfahrten",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      url: "https://timerse.com"
-    }
+        "Rudern ist Natur- und Erlebnissport. Das Befahren fremder Flüsse und Gewässer und das Beobachten von Natur und Tierwelt aus einer ungewohnten Perspektive bieten für Jung und Alt besondere Erfahrungen und Abenteuer.",
+      url: "https://timerse.com",
+    },
+    {
+      imageSrc: jugendsport,
+      subtitle: "",
+      title: "Jugendsport",
+      description:
+        "Spielerisch erlernen unsere Kids den Umgang mit dem Boot. Bootsmanöver, Rudertechnik, Rudern in verschiedenen Bootsklassen vom Einer bis zum Vierer stehen auf dem Programm. Koordinative Fähigkeiten, Teamgeist und Verantwortung werden so geschult.      Rudern ist ein Sport, der keine besonderen Vorkenntnisse erfordert und daher für Jugendliche leicht erlernbar. Mindestkörpergröße ca. 150 cm.",
+      url: "https://timerse.com",
+    },
+    {
+      imageSrc: wettkampfsport,
+      subtitle: "",
+      title: "Rudern als Wettkampfsport",
+      description: "Rudern ist ein Sport, bei dem Kraft, Ausdauer, Schnelligkeit und Beweglichkeit gesteigert werden. Wie kaum eine andere Sportart spricht Rudern eine breite Palette von Muskelgruppen an.",
+      url: "https://timerse.com",
+    },
   ];
 
   return (
@@ -79,7 +82,8 @@ export default () => {
         <HeadingInfoContainer>
           <HeadingTitle>Popular Events</HeadingTitle>
           <HeadingDescription>
-            Here are some of the most popular events in New York City curated by professionals.
+            Here are some of the most popular events in New York City curated by
+            professionals.
           </HeadingDescription>
         </HeadingInfoContainer>
 
@@ -97,10 +101,6 @@ export default () => {
           ))}
         </Content>
       </SingleColumn>
-      <SvgDotPattern1 />
-      <SvgDotPattern2 />
-      <SvgDotPattern3 />
-      <SvgDotPattern4 />
     </Container>
   );
 };
